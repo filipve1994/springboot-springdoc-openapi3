ddda1a840306b3cf65a152d7fdb29ab497fe42d2
Filip Vanden Eynde <filipve1994@gmail.com>
2019-11-21 14:55:06 +0100

    Initial commit

---
+++ src/main/java/com/filip/examples/springbootspringdocopenapi3/SpringbootSpringdocOpenapi3Application.java
@@ -0,0 +1,13 @@
+package com.filip.examples.springbootspringdocopenapi3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdocOpenapi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringdocOpenapi3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/filip/examples/springbootspringdocopenapi3/SpringbootSpringdocOpenapi3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip.examples.springbootspringdocopenapi3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringdocOpenapi3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
